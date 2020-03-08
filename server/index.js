f174b3129530e0432b9531bd89b69ef9bef7d84d
Esteban Mundo <estebanmundo1999@gmail.com>
2020-03-08 17:06:24 -0400

    Setup connection with MongoDB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
-
-
+require('dotenv').config();
 
 const PORT = 3001;
+
+/*********************************************************
+* Connecting to Mongo Database
+**********************************************************/
+var mongoose = require('mongoose');
+
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}`;
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log("Connection with MongoDB established");
+});
+
+db.on('error', () => {
+  console.error.bind(console, "MongoDB connection not established! :( ");
+})
+
+
+/*********************************************************
+* Setting up routes
+**********************************************************/
 
 app.get('/', (req,res) => {
   res.send("Connection made to API")
