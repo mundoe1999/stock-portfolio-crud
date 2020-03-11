49dea7fb1bb084c8fa18ed8c0944a7124ff8ab3f
Esteban Mundo <estebanmundo1999@gmail.com>
2020-03-11 00:45:58 -0400

    Added logout button

--- client/src/components/utils/NavBar.jsx
+++ client/src/components/utils/NavBar.jsx
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 class NavBar extends Component {
+  logout = () => {
+    Cookies.remove('user-data');
+  }
   render(){
     return (
       <nav>
         <Link to='/portfolio'>Portfolio</Link>
         <Link to='/transactions'>Transaction</Link>
-        <Link to='/logout'>Logout</Link>
+        <Link to='/' onClick={this.logout}>Logout</Link>
       </nav>
     )
   }
